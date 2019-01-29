53da708c1c5e4b04a632af55b2fbbf05e7748367
Viral Solani <viral.solani@gmail.com>
2019-01-29 11:43:57 +0530

    Register Globel components and add svg icons

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,8 +1,9 @@
 <template>
-   <router-link class="event-link" :to="{ name: 'event-show', params: { id: '1' } }"> 
+   <router-link class="event-link" :to="{ name: 'event-show', params: { id: '1' } }">
         <div class="event-card -shadow">
           <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
           <h4 class="title">{{ event.title }}</h4>
+          <BaseIcon name="users" />
           <span>{{ event.attendees.length }} attending</span>
         </div>
     </router-link>
